3da580ca78ae5d1cf78d41e819a6a131c9f3cd51
wh0smarg0 <wh0smarg0.s@gmail.com>
2025-11-28 14:41:59 +0200

    Update GoodsCard.js

--- lab7/src/components/GoodsCard.js
+++ lab7/src/components/GoodsCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function GoodsCard({ image, name, price }) {
   return (
-    <div className="goods-card" style={{ border: '1px solid #ccc', borderRadius: '8px', padding: '15px', width: '200px', textAlign: 'center', margin: '10px', background: '#fff' }}>
-      <img src={image} alt={name} style={{ width: '100px', height: '100px', objectFit: 'contain', marginBottom: '10px' }} />
-      <h3 style={{ fontSize: '18px', margin: '10px 0' }}>{name}</h3>
-      <p style={{ color: '#007bff', fontWeight: 'bold' }}>Ціна: {price} грн</p>
+    <div className="goods-card">
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <p>Ціна: {price} грн</p>
     </div>
   );
 }
